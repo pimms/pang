ad754574a9cebfb640171cfc78dccb0dded7620d
pimms <joakim.stien@gmail.com>
2014-02-02 20:05:57 +0100

    Added skeleton for opcode functions

--- pangvm/env.c
+++ pangvm/env.c
@@ -6,6 +6,10 @@
 
 
 
+static void execute_op(struct env*, uint8 *ops);
+
+
+
 uint
 arglen(uint8 op) 
 {
@@ -34,14 +38,79 @@ arglen(uint8 op)
 	return 0;
 }
 
+int 
+opcode_split(uint8 op, uint8 *body, uint8 *arg) 
+{
+	uint8 head = op & OP_MASK_HEAD;
+
+	if (head == OP_HEAD_STACKMEM) {
+		*body = op & OP_MASK_STACKMEM;
+		*arg = op & OP_MASK_STACKMEM_ARG;
+	} else if (head == OP_HEAD_FUNC) {
+		*body = op & OP_MASK_FUNC;
+		*arg = 0;
+	} else if (head == OP_HEAD_ARIT_CMP) {
+		*body = op & OP_MASK_ARIT_CMP;
+		*arg = op & OP_MASK_ARIT_CMP_32BIT;
+	} else {
+		return -1;
+	}
+
+	return 0;
+}
+
 
 void 
 execute(uint8 *opcodes, uint len) 
 {
-	
+	struct env *env = env_create();
+
+	while (env->reg->r_ip < len) {
+		execute_op(env, opcodes);
+	}
+
+	env_destroy(env);
 }
 
+static void
+execute_op(struct env *env, uint8 *ops)
+{
+	uint8 op;
+	uint8 body;
+	uint8 arg;
 
+	op = ops[env->reg->r_ip++];
+	opcode_split(op, &body, &arg);
+
+	switch (body) {
+		case OP_PUSH:		break;
+		case OP_POP:		break;
+		case OP_MOV:		break;
+
+		case OP_CALL:		break;
+		case OP_RET:		break;
+
+		case OP_ADD:		break;
+		case OP_SUB:		break;
+		case OP_MUL:		break;
+		case OP_DIV:		break;
+		case OP_MOD:		break;
+		case OP_LS:			break;
+		case OP_RS:			break;
+
+		case OP_XOR:		break;
+		case OP_AND:		break;
+		case OP_CMP:		break;
+
+		case OP_JMP:		break;
+		case OP_JE:			break;
+		case OP_JNE:		break;
+		case OP_JL:			break;
+		case OP_JLE:		break;
+		case OP_JG:			break;
+		case OP_JGE:		break;
+	}
+}
 
 
 struct regs*
@@ -80,4 +149,127 @@ env_destroy(struct env *env)
 {
 	regs_destroy(env->reg);
 	free(env);
+}
+
+
+
+/** OPCODE FUNCTIONS **/
+/* FUNC_OPCODE functions have this signature:
+ *		void f(struct env*, uint8 opcode, void* args)
+ *
+ * This WILL be fixed in the future. When I know that 
+ * these arguments solve all my problems. 
+ */
+FUNC_OPCODE(op_push) 
+{
+
+}
+
+FUNC_OPCODE(op_pop) 
+{
+
+}
+
+FUNC_OPCODE(op_mov) 
+{
+
+}
+
+
+FUNC_OPCODE(op_call) 
+{
+
+}
+
+FUNC_OPCODE(op_ret) 
+{
+
+}
+
+
+FUNC_OPCODE(op_add) 
+{
+
+}
+
+FUNC_OPCODE(op_sub) 
+{
+
+}
+
+FUNC_OPCODE(op_mul) 
+{
+
+}
+
+FUNC_OPCODE(op_div) 
+{
+
+}
+
+FUNC_OPCODE(op_mod) 
+{
+
+}
+
+FUNC_OPCODE(op_ls) 
+{
+
+}
+
+FUNC_OPCODE(op_rs) 
+{
+
+}
+
+
+FUNC_OPCODE(op_xor) 
+{
+
+}
+
+FUNC_OPCODE(op_and) 
+{
+
+}
+
+FUNC_OPCODE(op_cmp) 
+{
+
+}
+
+
+FUNC_OPCODE(op_jmp) 
+{
+
+}
+
+FUNC_OPCODE(op_je) 
+{
+
+}
+
+FUNC_OPCODE(op_jne) 
+{
+
+}
+
+FUNC_OPCODE(op_jl) 
+{
+
+}
+
+FUNC_OPCODE(op_jle) 
+{
+
+}
+
+FUNC_OPCODE(op_jg) 
+{
+
+}
+
+FUNC_OPCODE(op_jge) 
+{
+
 }
--- pangvm/env.h
+++ pangvm/env.h
@@ -8,6 +8,12 @@
  * external argument. */
 uint arglen(uint8 op);
 
+/* Puts the instruction without arguments in "body", and the
+ * arguments in "arg". 
+ * Returns 0 on success, -1 on error. 
+ */
+int opcode_split(uint8 opcode, uint8 *body, uint8 *arg);
+
 
 struct regs 
 {	
@@ -41,5 +47,40 @@ struct env
 
 struct env* env_create();
 void env_destroy(struct env *env);
+
+
+/* Opcode implementations are until they are known to function
+ * properly declared via this macro.
+ */
+#define FUNC_OPCODE(_FUNCTION)			\
+	void _FUNCTION(struct env *e, uint8 opcode, void *args)
+
+FUNC_OPCODE(op_push);
+FUNC_OPCODE(op_pop);
+FUNC_OPCODE(op_mov);
+
+FUNC_OPCODE(op_call);
+FUNC_OPCODE(op_ret);
+
+FUNC_OPCODE(op_add);
+FUNC_OPCODE(op_sub);
+FUNC_OPCODE(op_mul);
+FUNC_OPCODE(op_div);
+FUNC_OPCODE(op_mod);
+FUNC_OPCODE(op_ls);
+FUNC_OPCODE(op_rs);
+
+FUNC_OPCODE(op_xor);
+FUNC_OPCODE(op_and);
+FUNC_OPCODE(op_cmp);
+
+FUNC_OPCODE(op_jmp);
+FUNC_OPCODE(op_je);
+FUNC_OPCODE(op_jne);
+FUNC_OPCODE(op_jl);
+FUNC_OPCODE(op_jle);
+FUNC_OPCODE(op_jg);
+FUNC_OPCODE(op_jge);
+
 
 #endif /*__PANG_ENV_H__*/
